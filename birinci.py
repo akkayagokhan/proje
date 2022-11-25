5b9eb3815437695f9016f0a900dac750408b0374
akkayagokhan <032180094@ogr.uludag.edu.tr>
2022-11-25 14:00:03 +0300

    Create birinci.py

---
+++ birinci.py
@@ -0,0 +1,15 @@
+from lib2to3.pgen2 import pgen
+import pygame as pg
+import sys
+
+pg.init()
+
+en , boy = 800 , 600
+
+siyah = 0 , 0 , 0 , # RGB 0- 25
+beyaz = 255 ,255, 255
+kırmızı = 255 ,0, 0
+gri = 190 ,190,190
+cyan = 0 ,255, 255
+pink = 255 ,105, 180
+purple = 160 ,32, 24
